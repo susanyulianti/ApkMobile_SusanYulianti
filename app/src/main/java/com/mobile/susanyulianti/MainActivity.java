7a1c005533412307535c50fe52d0649087aefffd
noname-03 <fahrurrozi.arul3@gmail.com>
2021-11-12 10:03:07 +0700

    luas segitiga intent aritmatika

--- app/src/main/java/com/mobile/susanyulianti/MainActivity.java
+++ app/src/main/java/com/mobile/susanyulianti/MainActivity.java
@@ -2,13 +2,32 @@ package com.mobile.susanyulianti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button viewsegtiga;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //deklarasi ID
+        viewsegtiga = (Button) findViewById(R.id.segitiga);
+
+
+        //aksi listener atau intent
+        viewsegtiga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //memanggil class segitiga
+                startActivity(new Intent(getApplicationContext(),Segetiga.class));
+            }
+        });
     }
 }
