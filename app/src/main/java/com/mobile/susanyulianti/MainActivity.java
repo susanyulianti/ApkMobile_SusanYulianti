a7646ce7f5ce84693b0960ccd8bca8aeff1a5380
noname-03 <fahrurrozi.arul3@gmail.com>
2021-11-12 10:47:10 +0700

    radio button

--- app/src/main/java/com/mobile/susanyulianti/MainActivity.java
+++ app/src/main/java/com/mobile/susanyulianti/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button viewsegtiga;
+    private  Button viewpilih;
 
 
     @Override
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         //deklarasi ID
         viewsegtiga = (Button) findViewById(R.id.segitiga);
+        viewpilih = (Button)findViewById(R.id.pilih);
 
 
         //aksi listener atau intent
@@ -27,6 +31,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //memanggil class segitiga
                 startActivity(new Intent(getApplicationContext(),Segetiga.class));
+            }
+        });
+        //aksi radio
+        final RadioGroup rgbjenis = (RadioGroup) findViewById(R.id.rgjenis);
+        viewpilih.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = rgbjenis.getCheckedRadioButtonId();
+                switch (id){
+                    case R.id.rbj:
+                        Toast.makeText(getApplication(), "saya Suka Java", Toast.LENGTH_SHORT).show();
+                        break;
+                        case R.id.rbk:
+                        Toast.makeText(getApplication(), "saya Suka Kotlin", Toast.LENGTH_SHORT).show();
+                            break;
+                        case R.id.rbf:
+                        Toast.makeText(getApplication(), "saya Suka Flutter", Toast.LENGTH_SHORT).show();
+                            break;
+                }
             }
         });
     }
