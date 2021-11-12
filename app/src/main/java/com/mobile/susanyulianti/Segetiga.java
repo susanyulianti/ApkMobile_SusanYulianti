4fd7d7a6502d814a274b48f5805ae05aabdf373f
noname-03 <fahrurrozi.arul3@gmail.com>
2021-11-12 09:49:45 +0700

    frist commit

---
+++ app/src/main/java/com/mobile/susanyulianti/Segetiga.java
@@ -0,0 +1,14 @@
+package com.mobile.susanyulianti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Segetiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segetiga);
+    }
+}
